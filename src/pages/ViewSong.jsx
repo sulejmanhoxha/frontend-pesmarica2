1e40638da0853a3fe6afc8eb628d5560428a2223
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2023-07-09 03:35:07 +0200

    update viewsong for mobile

--- src/pages/ViewSong.jsx
+++ src/pages/ViewSong.jsx
@@ -32,35 +32,46 @@ const ViewSong = () => {
 		<>
 			<div className="container mx-auto  ">
 				<div className="mb-4 grid grid-cols-4 items-center gap-4 rounded-lg bg-slate-100 p-5">
-					<p className="col-span-1 text-lg font-bold">Artist</p>
+					<p className="col-span-2 text-lg font-bold md:col-span-1">
+						Artist
+					</p>
 					<Link
 						to={`/artists/${artistID}`}
-						className="col-span-3 underline underline-offset-4 hover:text-indigo-600"
+						className="col-span-2 underline underline-offset-4 hover:text-indigo-600 md:col-span-3"
 					>
 						{artistName}
 					</Link>
 				</div>
 
 				<div className="mb-4 grid grid-cols-4 items-center gap-4 rounded-lg bg-slate-100 p-5 ">
-					<p className="col-span-1 text-lg font-bold">Song name</p>
-					<p className="col-span-3"> {song.title}</p>
+					<p className="col-span-2 text-lg font-bold md:col-span-1">
+						Song name
+					</p>
+					<p className="col-span-2 md:col-span-3"> {song.title}</p>
 				</div>
 
 				<div className="mb-4 grid grid-cols-4 items-center gap-4 rounded-lg bg-slate-100 p-5 ">
-					<p className="col-span-1 text-lg font-bold">
+					<p className="col-span-2 text-lg font-bold md:col-span-1">
 						Year of release
 					</p>
-					<p className="col-span-3"> {song.year}</p>
+					<p className="col-span-2 md:col-span-3"> {song.year}</p>
 				</div>
 
 				<div className="mb-4 grid grid-cols-4 items-center gap-4 rounded-lg bg-slate-100 p-5 ">
-					<p className="col-span-1 text-lg font-bold">User</p>
-					<p className="col-span-3"> {song.owner}</p>
+					<p className="col-span-2 text-lg font-bold md:col-span-1">
+						User
+					</p>
+					<p className="col-span-2 md:col-span-3"> {song.owner}</p>
 				</div>
 
 				<div className="mb-4 grid grid-cols-4 items-center gap-4 rounded-lg bg-slate-100 p-5 ">
-					<p className="col-span-1 text-lg font-bold">Date created</p>
-					<p className="col-span-3"> {song.created_at}</p>
+					<p className="col-span-2 text-lg font-bold md:col-span-1">
+						Date created
+					</p>
+					<p className="col-span-2 md:col-span-3">
+						{" "}
+						{song.created_at}
+					</p>
 				</div>
 
 				<div className="mb-4 gap-8 rounded-lg bg-slate-100 p-5 ">
