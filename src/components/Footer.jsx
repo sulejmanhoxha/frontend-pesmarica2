18232f9babcc2a700699abccb99a948646d11f7d
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2023-07-12 05:56:10 +0200

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,19 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
 	return (
-		<footer className="mx-auto max-w-screen-xl bg-white px-4 py-5 text-gray-500 md:px-8">
+		<footer className="container mt-10 max-w-screen-xl bg-white py-5 text-gray-500">
 			<div className="max-w-lg sm:mx-auto sm:text-center">
-				<h2 className="w-32 bg-transparent text-2xl font-bold uppercase text-black sm:mx-auto">
+				<h2 className="mx-auto w-32 bg-transparent text-2xl font-bold uppercase text-black">
 					Guitarists
 				</h2>
-				<p className="mt-2 text-[15px] leading-relaxed">
-					Lorem Ipsum has been the industrys standard dummy text ever
-					since the 1500s, when an unknown printer took a galley of
-					type and scrambled it to make a type specimen book.
+				<p className="mt-2 text-center text-[15px] leading-relaxed">
+					Discover and learn guitar with our comprehensive collection
+					of songs, chords, and tutorials. Whether you&apos;re a
+					beginner or an experienced guitarist, we&apos;ve got you
+					covered. Join our community of guitar enthusiasts today!
 				</p>
 			</div>
-			<ul className="mt-8 items-center justify-center space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
+			<ul className="mt-8 flex items-center justify-center space-x-4  ">
 				<li key={1} className=" hover:text-gray-800">
 					<Link to="/">Home</Link>
 				</li>
